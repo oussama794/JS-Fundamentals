a93338cef59955b853213729e2b915c4ba6417bd
uuuhsama <85880019+oussama794@users.noreply.github.com>
2025-05-25 12:55:18 +0100

    Add files via upload

---
+++ 2-arguments.js
@@ -0,0 +1,10 @@
+const args = process.argv.slice(2);
+if (args.lengh === 0) {
+    console.log("No argument");
+}
+else if (args.lengh === 1) {
+    console.log("Argument found");
+}
+else {
+    console.log("Argument found");
+}
