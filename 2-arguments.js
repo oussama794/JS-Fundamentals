df466b7857abc3fdf451aa3517a3de2f628864c6
uuuhsama <85880019+oussama794@users.noreply.github.com>
2025-05-25 13:01:08 +0100

    Update 2-arguments.js
    fixing errors

--- 2-arguments.js
+++ 2-arguments.js
@@ -1,10 +1,10 @@
 const args = process.argv.slice(2);
-const count = args.length;
 
-if (count === 0) {
-  console.log("0 argument");
-} else if (count === 1) {
-  console.log("1 argument");
+if (args.length === 0) {
+  console.log("No argument");
+} else if (args.length === 1) {
+  console.log("Argument found");
 } else {
-  console.log(`${count} arguments`);
+  console.log("Arguments found");
 }
+
